c79c98a48392dc918861f660b9278b9b53a57023
Ilyosbek Nishonov <inishonov05@gmail.com>
2025-12-16 12:54:30 +0600

    Add implementations for problem C and problem D

--- problem_c.cpp
+++ problem_c.cpp
@@ -1,3 +1,121 @@
-//
-// Created by user on 12/15/2025.
-//
+#include <iostream>
+
+using namespace std;
+
+class Dictionary {
+private:
+    static const int MOD = 1000003;
+
+    struct Node {
+        long long key, value;
+        Node* next;
+        Node(long long k, long long v, Node* n)
+            : key(k), value(v), next(n) {}
+    };
+
+    Node* table[MOD];
+
+    int hashKey(long long k) {
+        return k % MOD;
+    }
+
+public:
+    Dictionary() {
+        for (int i = 0; i < MOD; i++)
+            table[i] = nullptr;
+    }
+
+    ~Dictionary() {
+        for (int i = 0; i < MOD; i++) {
+            Node* cur = table[i];
+            while (cur) {
+                Node* nxt = cur->next;
+                delete cur;
+                cur = nxt;
+            }
+        }
+    }
+
+    long long setValue(long long k, long long v) {
+        int h = hashKey(k);
+        Node* cur = table[h];
+
+        while (cur) {
+            if (cur->key == k) {
+                long long old = cur->value;
+                cur->value = v;
+                return old;
+            }
+            cur = cur->next;
+        }
+
+        table[h] = new Node(k, v, table[h]);
+        return -1;
+    }
+
+    long long removeValue(long long k) {
+        int h = hashKey(k);
+        Node* cur = table[h];
+        Node* prev = nullptr;
+
+        while (cur) {
+            if (cur->key == k) {
+                long long old = cur->value;
+                if (prev) prev->next = cur->next;
+                else table[h] = cur->next;
+                delete cur;
+                return old;
+            }
+            prev = cur;
+            cur = cur->next;
+        }
+
+        return -1;
+    }
+
+    long long getValue(long long k) {
+        int h = hashKey(k);
+        Node* cur = table[h];
+
+        while (cur) {
+            if (cur->key == k)
+                return cur->value;
+            cur = cur->next;
+        }
+
+        return -1;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q;
+    cin >> q;
+
+    Dictionary dict;
+
+    for (int i = 0; i < q; i++) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            long long k, v;
+            cin >> k >> v;
+            cout << dict.setValue(k, v) << ' ';
+        }
+        else if (type == 2) {
+            long long k;
+            cin >> k;
+            cout << dict.removeValue(k) << ' ';
+        }
+        else {
+            long long k;
+            cin >> k;
+            cout << dict.getValue(k) << ' ';
+        }
+    }
+
+    return 0;
+}
--- problem_d.cpp
+++ problem_d.cpp
@@ -1,3 +1,86 @@
-//
-// Created by user on 12/15/2025.
-//
+#include <iostream>
+
+using namespace std;
+
+class PriorityQueue {
+private:
+    long long *heap;
+    int sz;
+
+    void swap(long long &a, long long &b) {
+        long long t = a;
+        a = b;
+        b = t;
+    }
+
+    void siftUp(int i) {
+        while (i > 1 && heap[i] < heap[i / 2]) {
+            swap(heap[i], heap[i / 2]);
+            i /= 2;
+        }
+    }
+
+    void siftDown(int i) {
+        while (2 * i <= sz) {
+            int j = 2 * i;
+            if (j + 1 <= sz && heap[j + 1] < heap[j])
+                j++;
+            if (heap[i] <= heap[j]) return;
+            swap(heap[i], heap[j]);
+            i = j;
+        }
+    }
+
+public:
+    PriorityQueue(int capacity) {
+        heap = new long long[capacity + 1];
+        sz = 0;
+    }
+
+    ~PriorityQueue() {
+        delete[] heap;
+    }
+
+    void push(long long v) {
+        heap[++sz] = v;
+        siftUp(sz);
+    }
+
+    void popMin() {
+        if (sz == 0) return;
+        heap[1] = heap[sz--];
+        if (sz > 0)
+            siftDown(1);
+    }
+
+    long long getMin() {
+        return (sz == 0 ? -1 : heap[1]);
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int q;
+    cin >> q;
+
+    PriorityQueue pq(q);
+
+    for (int i = 0; i < q; i++) {
+        int type;
+        cin >> type;
+
+        if (type == 1) {
+            long long v;
+            cin >> v;
+            pq.push(v);
+        } else {
+            pq.popMin();
+        }
+
+        cout << pq.getMin() << ' ';
+    }
+
+    return 0;
+}
